63db04832da42a5836a2a6ad268caac2368b7465
Emanuele <manuncorrea@hotmail.com>
2020-02-23 10:37:41 -0300

    Inizialização do projeto blog

---
+++ README.md
@@ -0,0 +1,10 @@
+## Denpendencias instaladas:
+
+1. npm install react-router-dom    
+2. npm install --save firebase
+3. 
+
+## Arquivos inicial src:
+1. App.js
+2. index.js
+3. serviceWorker.js
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Home from './components/Home';
+import Header from './components/Header';
+import './global.css';
+
+class App extends Component{
+  render(){
+    return(
+      <BrowserRouter>
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
