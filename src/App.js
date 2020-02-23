af22548b18074a50be0c1098150061a7324d1640
Emanuele <manuncorrea@hotmail.com>
2020-02-23 11:18:39 -0300

    Introduzindo o Firebase no projeto blog

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, {Component} from 'react';
 import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import firebase from './firebase';
 
 import Home from './components/Home';
 import Header from './components/Header';
 import './global.css';
 
 class App extends Component{
+
+  state = {
+    firebaseInitialezed: false
+  };
+
+  componentDidMount(){
+    firebase.isInitialized().then(resultado =>{
+      //Devolve o usuario
+      this.setState({firebaseInitialezed: resultado});
+    })
+  }
+
   render(){
-    return(
+    return this.state.firebaseInitialezed !== false ?(
       <BrowserRouter>
         <Header/>
         <Switch>
           <Route exact path="/" component={Home} />
         </Switch>
       </BrowserRouter>
+    ) : (
+      <h1>Carregando...</h1>
     );
   }
 }
---
+++ src/firebase.js
@@ -0,0 +1,40 @@
+import app from 'firebase/app'
+import 'firebase/database';
+import 'firebase/auth';
+
+let firebaseConfig = {
+  apiKey: "AIzaSyAjfmSA_BcB1UXqV2pMfSvYSWm3Cxt2FRg",
+  authDomain: "react-app-b888f.firebaseapp.com",
+  databaseURL: "https://react-app-b888f.firebaseio.com",
+  projectId: "react-app-b888f",
+  storageBucket: "react-app-b888f.appspot.com",
+  messagingSenderId: "558130098473",
+  appId: "1:558130098473:web:0acca6255474590f240738",
+  measurementId: "G-ZBL7JFT3TE"
+};
+
+class Firebase{
+  constructor(){
+    app.initializeApp(firebaseConfig);
+  }
+
+  login(email, password){
+    return app.auth().signInWithEmailAndPassword(email, password)
+  }
+
+  async register(nome, email, password){
+    await app.auth().createUserWithEmailAndPassword(email, password)
+
+    const uid = app.database().ref('usuarios').child(uid).set({
+      nome: nome 
+    })
+  }
+
+  isInitialized(){
+    return new Promise(resolve =>{
+      app.auth().onAuthStateChanged(resolve);
+    })
+  }
+}
+
+export default new Firebase();
